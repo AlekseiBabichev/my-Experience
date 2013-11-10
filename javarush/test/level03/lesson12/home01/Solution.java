627d29961273b27aa17190f19f81a69c60b236c4
Aleksei <xxxl1001@yandex.ru>
2013-11-10 08:06:44 +0300

    gitignore

--- javarush/test/level03/lesson12/home01/Solution.java
+++
@@ -1,9 +0,0 @@
-package com.javarush.test.level03.lesson12.home01;
-
-public class Solution
-{
-    public static void main(String[] args)
-    {   String s = "Жить хорошо, а хорошо жить еще лучше";
-        System.out.print(s);
-    }
-}
--- javarush/test/level05/lesson12/home04/Cat.java
+++
@@ -1,8 +0,0 @@
-package com.javarush.test.level05.lesson12.home04;
-
-public class Cat extends Animal{
-    public static void staticMethod(){
-        System.out.println("Статический метод Cat");
-    }
-
-}
